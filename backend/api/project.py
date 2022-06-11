6dcf4ef5654584a54bea44d9bd5c03cb90cd9af9
Hyeonbin Chun <owen020405@gmail.com>
2022-06-10 20:11:15 -0400

    add link

--- backend/api/project.py
+++ backend/api/project.py
@@ -1,3 +1,4 @@
+from email.mime import base
 from flask import Blueprint, request
 from app import client
 import datetime
@@ -6,11 +7,19 @@ import requests
 import json
 from dotenv import load_dotenv
 import os
-
+import cloudinary
+# import cloudinary.uploader
+from cloudinary import uploader
 load_dotenv()
 
 project = Blueprint("user", __name__)  # initialize blueprint
 db = client.pizza
+
+cloudinary.config(
+    cloud_name="dz03b1a69",
+    api_key= os.getenv('CLOUDINARY_API_KEY'),
+    api_secret=os.getenv('CLOUDINARY_API_SECRET')
+)
 
 
 # function that is called when you visit /
@@ -18,14 +27,16 @@ db = client.pizza
 def get_tags():
     # Receive an image
     imagefile = request.files.get('img', '')
-
+    file_to_upload = request.files.get("file", '')
+    # imagefile = file_to_upload
     # Convert image to base64
     base64_img = b64encode(imagefile.read())
-    print(str(base64_img)[0:10])
 
     # Pass image base64 data to Google Vision API (refer to postman)
 
-    url = "https://vision.googleapis.com/v1/images:annotate?key=" + os.getenv('GOOGLE_API')
+    url = "https://vision.googleapis.com/v1/images:annotate?key=" + \
+        os.getenv('GOOGLE_API')
+
 
     payload = json.dumps({
         "requests": [
@@ -61,5 +72,18 @@ def get_tags():
         msg: "success"
     }
     """
+    # file_to_upload = 'data:image/png;base64,{}'.format(base64_img[2:])
+    # print(base64_img[:30])
+    # print(file_to_upload[:30])
+    res = cloudinary.uploader.upload(file_to_upload)
 
-    return {"tags": lst, "msg": "success"}
+    #1. Get img link from res
+    image_link = res['url']
+    print(image_link)
+
+    #2. insert image link and tags into mongodb (to do this, you need to setup mongodb client inside this file)
+    user_data = {"tags": lst, "image_link": image_link}
+    db.images.insert_one(user_data)
+    return {
+        "msg": "success"
+    }
